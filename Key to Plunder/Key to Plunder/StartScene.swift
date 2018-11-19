1be59eb2d658946f6789825c5050d491727e8c4c
Scott Shelton <shelton.scott25f@outlook.com>
2018-11-19 11:13:41 -0700

    Tile sets

---
+++ Key to Plunder/Key to Plunder/StartScene.swift
@@ -0,0 +1,42 @@
+//
+//  GameScene.swift
+//  Key to Plunder
+//
+//  Created by scott shelton on 11/19/18.
+//  Copyright © 2018 scott shelton. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class StartScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
